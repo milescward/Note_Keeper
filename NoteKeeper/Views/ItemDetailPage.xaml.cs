5b59606271805985d21ef315f5d0eb539917cee4
miles.c.ward@gmail.com <miles.c.ward@gmail.com>
2019-11-13 19:55:18 -0800

    Initial check-in of module NoteKeeper

---
+++ NoteKeeper/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using NoteKeeper.Models;
+using NoteKeeper.ViewModels;
+using System.Collections.Generic;
+using NoteKeeper.Services;
+
+namespace NoteKeeper.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            this.viewModel = viewModel;
+            BindingContext = this.viewModel;
+        }
+
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+
+            viewModel = new ItemDetailViewModel();
+            BindingContext = viewModel;
+        }
+
+        public void Cancel_Clicked(Object sender, EventArgs eventArgs)
+        {
+            viewModel.NoteHeading = "XXXXXXX";
+            DisplayAlert("Cancel option", "heading value is " + viewModel.Note.Heading, "B2", "B1" );
+        }
+
+        public void Save_Clicked(Object sender, EventArgs eventArgs)
+        {
+            DisplayAlert("Save option", "Save?", "Button2", "Button1");
+        }
+
+    }
+}
